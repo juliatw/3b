58a515e7c15968e31c38e8e85c3adb5862fa9b1f
juliatw <161319400+juliatw@users.noreply.github.com>
2025-03-26 08:58:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -23,3 +23,9 @@ document.addEventListener('DOMContentLoaded', function(){
        })
     });
 
+    
+    ScrollReveal().reveal('#inicio', { delay: 500 });
+    ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });s
